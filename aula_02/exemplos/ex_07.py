5a9e40ac8af4bed09f0d9eae34e9cd1a73734e47
daniel.sena <daniel.sena@tds.company>
2024-04-04 21:05:41 -0300

    adicionando execício de calculo de média

---
+++ aula_02/exemplos/ex_07.py
@@ -0,0 +1,8 @@
+nota1 = float(input("digite a nota 1"))
+nota2 = float(input("digite a nota 2"))
+
+media = (nota1 + nota2) / 2
+if media >= 7:
+    print("Aprovado(a)")
+else:
+    print("Reprovado")
