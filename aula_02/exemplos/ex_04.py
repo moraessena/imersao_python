7af4dc694c76ff4447e0583573cf87453baf03c4
daniel.sena <daniel.sena@tds.company>
2024-04-04 21:01:51 -0300

    adicionando else

--- aula_02/exemplos/ex_04.py
+++ aula_02/exemplos/ex_04.py
@@ -1,4 +1,5 @@
 mesada = float(input("Quanto você ganhou de mesada? "))
 if mesada >= 50:
     print(f"Com R$ {mesada} é possível ir ao cinema!")
-print("Peça mais dinheiro a sua mãe Kiko")
+else:
+    print("Peça mais dinheiro a sua mãe Kiko")
